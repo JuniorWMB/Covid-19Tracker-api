72369f3ce49c58e71c588c1984bd70a84becda18
Junior <juniormbaya243@gmail.com>
2020-09-22 22:51:48 +0200

    add react leaflet

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,7 +1,96 @@
 import React, { useState, useEffect } from "react";
+import numeral from "numeral";
 import { Line } from "react-chartjs-2";
 
-function LineGraph() {
+// const options = {
+//     legend: {
+//       display: false,
+//     },
+//     elements: {
+//       point: {
+//         radius: 0,
+//       },
+//     },
+//     maintainAspectRatio: false,
+//     tooltips: {
+//       mode: "index",
+//       intersect: false,
+//       callbacks: {
+//         label: function (tooltipItem, data) {
+//           return numeral(tooltipItem.value).format("+0,0");
+//         },
+//       },
+//     },
+//     scales:{
+//       xAxes:[
+//           {
+//               type:"time",
+//               time:{
+//                   format:"MM/DD/YY",
+//                   tooltipFormat:"ll",
+//               },
+//           },
+//       ],
+//       yAxes:[
+//           {
+//               gridLines:{
+//                   display:false,
+//               },
+//               ticks:{
+//                   callback:function(value,index,values){
+//                       return numeral(value).format("0a");
+//                   },
+//               },
+//           },
+//       ],
+//   },
+//   },
+
+const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          format: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
+
+function LineGraph({ casesType = "cases" }) {
   const [data, setData] = useState({});
 
   const buildChartData = (data, casesType = "cases") => {
@@ -22,30 +111,35 @@ function LineGraph() {
   };
 
   useEffect(() => {
-    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=30")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("LINE GRAPH HERE", data);
-        const chartData = buildChartData(data);
-        setData(chartData);
-      });
-  }, []);
+    const fetchData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=30")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("LINE GRAPH HERE", data);
+          const chartData = buildChartData(data);
+          setData(chartData);
+        });
+    };
+    fetchData();
+  }, [casesType]);
 
   return (
     <div>
       <h1>Im a graph</h1>
-      <Line
-        data={{
-          datasets: [
-            {
-              backgroundColor: "rgba(204,16,52,0.5)",
-              borderColor: "#CC1034",
-              data: data,
-            },
-          ],
-        }}
-        options
-      />
+      {data?.length > 0 && (
+        <Line
+          options={options}
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204,16,52,0.5)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+        />
+      )}
     </div>
   );
 }
